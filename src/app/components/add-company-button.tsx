35268189f0278e920aafd4ae534c1a448eda6ce5
Risina Liliia <risinalilia2@gmail.com>
2025-10-05 10:49:02 +0200

    fix form add company

--- src/app/components/add-company-button.tsx
+++ src/app/components/add-company-button.tsx
@@ -1,14 +1,22 @@
 'use client';
 
-import React from 'react';
-import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import dynamic from 'next/dynamic';
 import Button from '@/app/components/button';
 
+const CompanyFormModal = dynamic(() => import('./company-form-modal'), {
+    ssr: false,
+});
+
 export default function AddCompanyButton() {
-  const router = useRouter();
-  return (
-    <Button onClick={() => router.push('/companies/new', { scroll: false })}>
-      Add company
-    </Button>
-  );
+    const [show, setShow] = useState(false);
+    return (
+        <>
+            <Button onClick={() => setShow(true)}>Add company</Button>
+            <CompanyFormModal
+                show={show}
+                onClose={() => setShow(false)}
+            />
+        </>
+    );
 }
